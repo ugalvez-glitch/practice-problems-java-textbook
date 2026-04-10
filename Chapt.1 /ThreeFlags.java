91efddd72351ad197408e440f5687717228acb31
ugalvez-glitch <ugalvez@yahoo.com>
2026-04-10 15:02:50 -0700

    Added ThreeFlags

---
+++ Chapt.1 /ThreeFlags.java
@@ -0,0 +1,46 @@
+public class Mississippi {
+	public static void main(String[] args) {
+	firstFlag();
+	threeLineSpace();
+	secondFlag();
+	threeLineSpace();
+	thirdFlag();
+}
+	public static void lineStar() {
+		System.out.println("*****");
+	}
+	
+	public static void twoPointStar() {
+		System.out.println(" * * ");
+	}
+	
+	public static void middleStar() {
+		System.out.println("  *   ");
+	}
+	
+	public static void threeLineSpace() {
+		System.out.println("     ");
+		System.out.println("     ");
+		System.out.println("     ");
+	}
+	public static void firstFlag() {
+		lineStar();
+		lineStar();
+		twoPointStar();
+		middleStar();
+		twoPointStar();
+	}
+	
+	public static void secondFlag() {
+		firstFlag();
+		lineStar();
+		lineStar();
+	}
+	
+	public static void thirdFlag() {
+		middleStar();
+		middleStar();
+		middleStar();
+		firstFlag();
+	}
+}
